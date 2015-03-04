c8776a8172e1a25a052ba670481b58513e02f799
Jordan Hawker <hawker.jordan@gmail.com>
2015-03-04 11:38:29 -0800

    Initial version

---
+++ addon/components/bug-widget.js
@@ -0,0 +1,35 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  hide: true,
+  horizontal: 'right',
+  vertical: 'bottom',
+  bugIcon: 'fa fa-bug',
+  arrowLeftIcon: 'fa fa-angle-double-left',
+  arrowRightIcon: 'fa fa-angle-double-right',
+
+  hiddenClass: function () {
+    if (this.get('hide')) {
+      return 'hide';
+    }
+
+    return '';
+  }.property('hide'),
+
+  arrowDirection: function () {
+    var hide = this.get('hide');
+    var left = this.get('arrowLeftIcon');
+    var right = this.get('arrowRightIcon');
+
+    if (this.get('horizontal') === 'right') {
+      return hide ? left : right;
+    }
+    return hide ? right : left;
+  }.property('hide', 'arrowLeftIcon', 'arrowRightIcon'),
+
+  actions: {
+    toggle: function () {
+      this.toggleProperty('hide');
+    }
+  }
+});
---
+++ app/components/bug-widget.js
@@ -0,0 +1,4 @@
+import Ember from 'ember';
+import BugWidget from 'ember-bug-widget/components/bug-widget';
+
+export default BugWidget;
