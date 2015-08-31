d55492ca0cd5fee7227c25a0f48a4b0236786e1b
Jordan Hawker <hawker.jordan@gmail.com>
2015-08-31 13:33:13 -0700

    Integrate ember-cli-github-pages 1.13.9 branch

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'dummy',
     environment: environment,
-    baseURL: '/',
+    baseURL: '/ember-bug-widget',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
